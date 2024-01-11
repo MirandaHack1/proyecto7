ace01066d543fa1d9f138af634a6dea1943b8727
MirandaHack1 <miranda3791167@gmail.com>
2024-01-11 16:50:23 -0500

    Proyecto7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,43 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'proyecto7';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+
+  numero1 = 1;
+  numero2 = 2;
+
+  lanzardados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+  }
+
+  /*aplicar_sonido(numero: number): void {
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }*/
 }
+/**
+ * var funcion = ()=>{}
+ * function nombre(){}
+ * nombres(){}
+ * ()=>{}
+ *
+ *
+ * let nombre = '';
+ *
+ * Typescript
+ *
+ * nombre:Tipo de Dato
+ * nombre:string
+ *
+ *
+ */
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DadosComponent } from './dados/dados.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'dados', 
+        component: DadosComponent,
+    },
+];
